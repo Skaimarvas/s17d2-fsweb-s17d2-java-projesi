9c67259216d2a3984a83f8975036a4d997e1cd96
skaimarvas <skaimarvas@gmail.com>
2024-02-06 17:33:02 +0300

    @Component annotation was added to DeveloperTax

--- dependencyInjection/src/main/java/com/example/dependencyInjection/tax/DeveloperTax.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/tax/DeveloperTax.java
@@ -3,7 +3,13 @@ package com.example.dependencyInjection.tax;
 import com.example.dependencyInjection.model.JuniorDeveloper;
 import com.example.dependencyInjection.model.MidDeveloper;
 import com.example.dependencyInjection.model.SeniorDeveloper;
+import org.springframework.stereotype.Component;
 
+
+/**
+ * @Component anotasyonu koymadığım için bean? hatası aldım.
+ */
+@Component
 public class DeveloperTax implements Taxable {
 
     private JuniorDeveloper juniorDeveloper;
