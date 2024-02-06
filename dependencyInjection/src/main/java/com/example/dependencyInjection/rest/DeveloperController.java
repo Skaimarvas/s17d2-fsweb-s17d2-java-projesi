91955fd8b216d61aafa1d5d90e9669f985983b60
skaimarvas <skaimarvas@gmail.com>
2024-02-06 15:07:42 +0300

    developers map was created in DeveloperController

--- dependencyInjection/src/main/java/com/example/dependencyInjection/rest/DeveloperController.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/rest/DeveloperController.java
@@ -1,10 +1,14 @@
 package com.example.dependencyInjection.rest;
 
+import com.example.dependencyInjection.model.Developer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/developer")
 public class DeveloperController {
+  private Map<Integer, Developer> developers;
 
 }
