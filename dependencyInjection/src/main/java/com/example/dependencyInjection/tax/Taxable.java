8f4efb90c8a7fa5c2dffa5d2b0c0357377fee476
skaimarvas <skaimarvas@gmail.com>
2024-02-06 14:57:28 +0300

    Taxable with its methods was created

---
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/tax/Taxable.java
@@ -0,0 +1,8 @@
+package com.example.dependencyInjection.tax;
+
+public interface Taxable {
+
+    abstract double getSimpleTaxRate();
+    abstract double getMiddleTaxRate();
+    abstract double getUpperTaxRate();
+}
