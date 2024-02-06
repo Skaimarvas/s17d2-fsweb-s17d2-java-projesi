89f8127a5bf418abcebfd9f4033d17d06f415f10
skaimarvas <skaimarvas@gmail.com>
2024-02-06 17:33:55 +0300

    @Component annotation was added to Developer class

--- dependencyInjection/src/main/java/com/example/dependencyInjection/model/Developer.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/model/Developer.java
@@ -1,5 +1,8 @@
 package com.example.dependencyInjection.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Developer {
     private int id;
     private String name;
