09dbfca7b1b36cfe662c6abaa9f32c7f9646d441
skaimarvas <skaimarvas@gmail.com>
2024-02-06 14:33:40 +0300

    salary and experience parameters were deleted in SeniorDeveloper constructor

--- dependencyInjection/src/main/java/com/example/dependencyInjection/model/SeniorDeveloper.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/model/SeniorDeveloper.java
@@ -1,7 +1,7 @@
 package com.example.dependencyInjection.model;
 
 public class SeniorDeveloper extends Developer {
-    public SeniorDeveloper(int id, String name, int salary, Experience experience) {
+    public SeniorDeveloper(int id, String name) {
         super(id, name, 8000, Experience.SENIOR);
     }
 }
