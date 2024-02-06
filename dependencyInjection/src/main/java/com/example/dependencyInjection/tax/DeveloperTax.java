bde3a99c1ed8b650fea1f971c7af9093de5420e0
skaimarvas <skaimarvas@gmail.com>
2024-02-06 14:56:47 +0300

    DeveloperTax class with variables and methods was created

---
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/tax/DeveloperTax.java
@@ -0,0 +1,28 @@
+package com.example.dependencyInjection.tax;
+
+import com.example.dependencyInjection.model.JuniorDeveloper;
+import com.example.dependencyInjection.model.MidDeveloper;
+import com.example.dependencyInjection.model.SeniorDeveloper;
+
+public class DeveloperTax implements Taxable {
+
+    private JuniorDeveloper juniorDeveloper;
+    private MidDeveloper midDeveloper;
+    private SeniorDeveloper seniorDeveloper;
+
+
+    @Override
+    public double getSimpleTaxRate() {
+        return juniorDeveloper.getSalary() * 0.05 ;
+    }
+
+    @Override
+    public double getMiddleTaxRate() {
+        return midDeveloper.getSalary() * 0.10;
+    }
+
+    @Override
+    public double getUpperTaxRate() {
+        return seniorDeveloper.getSalary()* 0.15;
+    }
+}
