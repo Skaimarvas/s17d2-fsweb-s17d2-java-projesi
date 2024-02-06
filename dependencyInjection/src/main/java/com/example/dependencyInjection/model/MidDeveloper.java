7a90323a838d9d240f54334246f5d7db52100e87
skaimarvas <skaimarvas@gmail.com>
2024-02-06 14:33:16 +0300

    salary and experience parameters were deleted in MidDeveloper constructor

--- dependencyInjection/src/main/java/com/example/dependencyInjection/model/MidDeveloper.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/model/MidDeveloper.java
@@ -1,7 +1,7 @@
 package com.example.dependencyInjection.model;
 
 public class MidDeveloper extends Developer{
-    public MidDeveloper(int id, String name, int salary, Experience experience) {
+    public MidDeveloper(int id, String name) {
         super(id, name, 6000, Experience.MID);
 
     }
