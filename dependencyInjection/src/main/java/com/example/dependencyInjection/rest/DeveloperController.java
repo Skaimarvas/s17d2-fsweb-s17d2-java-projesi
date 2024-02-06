255be4b1dd88f6e96eeb630ce5f4e49342721155
skaimarvas <skaimarvas@gmail.com>
2024-02-06 17:00:15 +0300

    Taxable added via @Autowired annotation

--- dependencyInjection/src/main/java/com/example/dependencyInjection/rest/DeveloperController.java
+++ dependencyInjection/src/main/java/com/example/dependencyInjection/rest/DeveloperController.java
@@ -1,14 +1,38 @@
 package com.example.dependencyInjection.rest;
 
 import com.example.dependencyInjection.model.Developer;
+import com.example.dependencyInjection.model.JuniorDeveloper;
+import com.example.dependencyInjection.model.MidDeveloper;
+import com.example.dependencyInjection.model.SeniorDeveloper;
+import com.example.dependencyInjection.tax.Taxable;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+/**
+ * CrossOrigin neden kullanılır?
+ */
+
 @RestController
 @RequestMapping("/developer")
 public class DeveloperController {
   private Map<Integer, Developer> developers;
+
+  @Autowired
+  private Taxable tax;
+
+  @PostConstruct
+  void init(){
+    developers.put(1, new JuniorDeveloper(1,"Micheal Jackson"));
+    developers.put(2, new MidDeveloper(2,"Jason Derulo"));
+    developers.put(3, new SeniorDeveloper(3,"Mikhail Gorbachev"));
+  }
+
+
+
+
 
 }
